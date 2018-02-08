3743c9383015207afc92149ff6b91cccb37e8957
eriklarson33 <eriklarson33@msn.com>
2018-02-08 13:38:40 -0600

    First Commit

---
+++ src/model/SuperHuman.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author eriklarson-laptop
+ */
+public class SuperHuman {
+    
+    private int superHumanId;
+    private String superHumanName;
+    private String description;
+
+    // Getters & Setters
+    public int getSuperHumanId() {
+        return superHumanId;
+    }
+
+    public void setSuperHumanId(int superHumanId) {
+        this.superHumanId = superHumanId;
+    }
+
+    public String getSuperHumanName() {
+        return superHumanName;
+    }
+
+    public void setSuperHumanName(String superHumanName) {
+        this.superHumanName = superHumanName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
